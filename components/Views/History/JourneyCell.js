33e835021d9a595626bd9b62e2cab1da36cf48a2
sgaga <a0920336610@gmail.com>
2017-09-21 14:34:13 +0800

    journey ok

---
+++ components/Views/History/JourneyCell.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { ScrollView,StyleSheet,View,Text,Image } from 'react-native';
+import { Tile, List, ListItem } from 'react-native-elements';
+import moment from 'moment';
+
+// Make a component
+class HisSendjourneyCardconst extends Component {
+  state = { hissendjourneycardconst: [] };
+
+
+  render() {
+    const {container,leftstyle,lefttextstyle,centerstyle,rightstyle,textstyle,whitelineUP,Round,whitelineDown} = styles;
+    return (
+      <View style={container}>
+        <View style={leftstyle}>
+          <View style={lefttextstyle}>
+            <Text style={textstyle}>{moment(moment.now()).format('YYYY/MM/DD')}</Text>
+          </View>
+          <View style={lefttextstyle}>
+            <Text style={textstyle}>{moment(moment.now()).format('hh:mm')}</Text>
+          </View>          
+        </View>
+        <View style={centerstyle}>
+          <View style={whitelineUP}></View>
+          <View style={Round}></View>            
+          <View style={whitelineDown}></View>          
+        </View>
+        <View style={rightstyle}>
+          <View>
+            <Text style={textstyle}>台灣 / 台中市</Text>            
+          </View>
+        </View>                
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container:{
+    alignItems:'center',
+    flexDirection:'row',
+  },
+  leftstyle:{
+    width:70,
+  },
+  lefttextstyle:{
+    alignItems:'flex-end',
+  },
+  centerstyle:{
+    alignItems:'center',
+    width:60,
+  },
+  rightstyle:{
+    width:100,
+    marginLeft: 12,
+  },
+  textstyle:{
+    fontSize: 12,
+    color:'#fefefe',
+  },
+  whitelineUP:{    
+    width:3,
+    height:33,
+    backgroundColor:'#fefefe',
+  },
+  Round:{
+    backgroundColor:'#fefefe',
+    width: 10,
+    height:10,
+    borderStyle:'solid',
+    borderRadius:5.4,
+  },
+  whitelineDown:{
+    width:3,
+    height:33,
+    backgroundColor:'#fefefe',
+  },
+})
+export default HisSendjourneyCardconst;
--- components/Views/History/JourneyView.js
+++ components/Views/History/JourneyView.js
@@ -1,1 +1,173 @@
 //送出船的經歷
+import React, { Component } from 'react';
+import { ScrollView, StyleSheet, View, Text, Image } from 'react-native';
+import { Tile, List, ListItem } from 'react-native-elements';
+import JourneyCell from './JourneyCell';
+import JourneyTitle from './JourneyTitle';
+
+// Make a component
+class HistorySendJourney extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { hissendjourney: [] };
+    }
+
+    render() {
+        const { container, headerstyle,headerstyleB,headerstyleC, conststyle, Roundin, Roundout, headerBGC,
+            Roundwidth, leftspace, rightspace, textstyle, titletext, whiteline, whitelineWidth, whitelineB } = styles;
+        return (
+
+            <View style={container}>
+                <ScrollView style={container}>
+                    <View style={headerBGC}>
+                        <View style={headerstyle}>
+                            {/* 左方調整匡 */}
+                            <View style={leftspace}></View>
+                            <View style={Roundwidth}>
+                                <View style={Roundout}>
+                                    <View style={Roundin}>
+                                        <Image source={require('../../../assets/send/bboat.png')} />
+                                    </View>
+                                </View>
+                            </View>
+                            {/*圖片結束*/}
+                            <View style={textstyle}>
+                                <View>
+                                    <Text style={titletext}>台灣 / 台北市</Text>
+                                </View>
+                                <View>
+                                    <Text style={titletext}>2017/07/17   21:43</Text>
+                                </View>
+                            </View>
+                        </View>
+                        {/*下面的槓槓*/}
+                        <View style={headerstyle}>
+                            {/* 左方調整匡 */}
+                            <View style={leftspace}></View>
+                            <View style={whitelineWidth}><View style={whiteline}></View></View>
+                            {/* 右方調整匡 */}
+                            <View style={rightspace}></View>
+                        </View>
+                    </View>
+                    {/* JourneyCell */}
+                    <View style={headerstyleB}>
+                        <JourneyCell />
+                        <JourneyCell />
+                        <JourneyCell />
+                        <JourneyCell />
+                        <JourneyCell />
+                        <JourneyCell />
+                        <JourneyCell />
+                        <JourneyCell />
+                        <JourneyCell />
+                        <JourneyCell />
+                        <JourneyCell />
+                        <JourneyCell />
+                        <JourneyCell />
+                        <JourneyCell />
+                        <JourneyCell />
+                        <JourneyCell />
+                    </View>
+                    {/* 最下面的槓槓 */}
+                    <View style={headerstyleC}>
+                        {/* 左方調整匡 */}
+                        <View style={leftspace}></View>
+                        <View style={whitelineWidth}>
+                            <View style={whitelineB}></View>
+                            <View style={whitelineB}></View>
+                            <View style={whitelineB}></View>
+                            <View style={whitelineB}></View>
+                            <View style={whitelineB}></View>
+                        </View>
+                        {/* 右方調整匡 */}
+                        <View style={rightspace}></View>
+                    </View>
+                </ScrollView>
+            </View>
+        );
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#56abc8',
+        // alignItems: 'center',
+        flex: 1,
+    },
+    headerBGC: {
+        alignItems: 'center',
+        width: '100%',
+        backgroundColor: '#106085',
+        paddingTop: 40,
+    },
+    leftspace: {
+        width: 70,
+        height: 20,
+    },
+    rightspace: {
+        width: 100,
+        marginLeft: 12,
+        height: 20,
+    },
+    headerstyle: {
+        alignItems: 'center',
+        flexDirection: 'row',
+    },
+    headerstyleB:{
+        alignItems: 'center',
+    },
+    headerstyleC:{
+        justifyContent:'center',
+        flexDirection: 'row',
+    },
+    Roundwidth: {
+        width: 60,
+    },
+    Roundin: {
+        backgroundColor: '#fefefe',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 50,
+        height: 50,
+        borderStyle: 'solid',
+        borderRadius: 26.8,
+        paddingBottom: 2,
+    },
+    Roundout: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 60,
+        height: 60,
+        borderWidth: 1,
+        borderColor: '#fefefe',
+        borderStyle: 'solid',
+        borderRadius: 32.2,
+    },
+    conststyle: {
+        backgroundColor: '#56abc8',
+        flex: 1,
+    },
+    textstyle: {
+        width: 100,
+        marginLeft: 12,
+    },
+    titletext: {
+        fontSize: 14,
+        color: '#fefefe',
+    },
+    whitelineWidth: {
+        width: 60,
+        alignItems: 'center',
+    },
+    whiteline: {
+        width: 3,
+        height: 22,
+        backgroundColor: '#fefefe',
+    },
+    whitelineB: {
+        width: 3,
+        height: 10,
+        backgroundColor: '#fefefe',
+        marginTop: 5,
+    }
+})
+export default HistorySendJourney;
